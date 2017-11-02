22943db407ad508ba94c80c951aa4f5d21dc8f8f
OliverEduardo <OliverEduardo@OliverAlmonte>
2017-11-02 11:02:45 -0400

    probando

--- FinalP1/src/logico/prueba.java
+++ FinalP1/src/logico/prueba.java
@@ -1,5 +1,20 @@
 package logico;
 
 public class prueba {
+	private int probando;
+	private String nombre;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getProbando() {
+		return probando;
+	}
+	public void setProbando(int probando) {
+		this.probando = probando;
+	}
 
 }
